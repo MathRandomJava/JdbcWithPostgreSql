90b934dffd6952cecfe43e09b23e7ccd38a0eab6
Trickster <dmc.trickster@gmail.com>
2020-01-25 14:58:04 +0500

    add create table method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
 
@@ -10,28 +11,42 @@ public class Main {
 
     private final static String PSWD = "root";
 
+    private final static String SQL = "create table if not exists users ( id numeric, name varchar(20) )";
+
     public static void main(String[] args) {
 
+        createUserTable();
+
+    }
+
+    private static Connection getConnection() {
+        Connection connection = null;
         try {
             Class.forName("org.postgresql.Driver");
+
+            connection = DriverManager.getConnection(URL, USER, PSWD);
         } catch (ClassNotFoundException e) {
             System.out.println("PostgreSQL JDBC Driver is not found. Include it in your library path");
             e.printStackTrace();
-            return;
-        }
-
-        System.out.println("PostgreSQL JDBC Driver successfully connected");
-        try (Connection connection = DriverManager
-                .getConnection(URL, USER, PSWD)) {
-
-           if (connection != null) {
-                System.out.println("Connection successfully");
-            } else {
-                System.out.println("Connection failed");
-            }
         } catch (SQLException e) {
             System.out.println("Connection Failed");
             e.printStackTrace();
+        }
+        return connection;
+    }
+
+    private static void createUserTable() {
+        Connection connection = getConnection();
+        if (connection != null) {
+
+            try (Statement statement = connection.createStatement()) {
+                statement.execute(SQL);
+                System.out.println("Table users created");
+            } catch (SQLException e) {
+                e.printStackTrace();
+                System.out.println("Creating table is failed");
+            }
+
         }
     }
 }
